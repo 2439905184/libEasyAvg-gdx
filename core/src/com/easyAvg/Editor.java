f6435868406dd777b3c008d3dcb1ee7c6508910e
2439905184 <suchenglong018@126.com>
2021-10-06 13:27:59 +0800

    添加了编辑界面初步

--- core/src/com/easyAvg/Editor.java
+++ core/src/com/easyAvg/Editor.java
@@ -1,14 +1,49 @@
 package com.easyAvg;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Button;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.ui.Tree;
+import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
 
 public class Editor
 {
     //启动的工程管理界面
-    public void create_project_view()
+    public void create_project_view(Stage stage, BitmapFont font)
     {
-        Button b_start = new Button();
-        b_start.setName("启动工程");
-
+        Label.LabelStyle style = new Label.LabelStyle(font,font.getColor());
+        Label title = new Label("Projet Manager",style);
+        title.setX(Gdx.graphics.getWidth()/2);
+        title.setY(500);
+        stage.addActor(title);
+        TextButton.TextButtonStyle btn_style = new TextButton.TextButtonStyle(null,null,null,font);
+        TextButton btn_edit = new TextButton("edit",btn_style);
+        btn_edit.setX(700);
+        btn_edit.setY(300);
+        stage.addActor(btn_edit);
+    }
+    public void create_main_view(Stage stage,BitmapFont font)
+    {
+        System.out.println("created main view!");
+        stage.clear();
+        Tree tree;
+        //创建事件表UI
+        Label.LabelStyle style = new Label.LabelStyle(font,font.getColor());
+        Label title = new Label("EventTable",style);
+        title.setY(50);
+        stage.addActor(title);
+        TextButton.TextButtonStyle btn_style = new TextButton.TextButtonStyle(null,null,null,font);
+        TextButton btn_add_event = new TextButton("add Event",btn_style);
+        TextButton btn_group = new TextButton("按钮组",btn_style);
+        Texture t = new Texture("badlogic.jpg");
+        btn_group.setBackground("badlogic.jpg");
+        btn_group.setX(200);
+        stage.addActor(btn_add_event);
+        stage.addActor(btn_group);
     }
 }
--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -10,43 +10,71 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.graphics.g2d.freetype.FreeType;
 import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
+import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.easyAvg.Editor;
 
 public class MyGdxGame extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
-	BitmapFont font;
+
 	OrthographicCamera cam;
+	Stage stage;
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-		FreeTypeFontGenerator generator = new FreeTypeFontGenerator(Gdx.files.internal("m5x7.ttf"));
-		FreeTypeFontGenerator.FreeTypeFontParameter parameter = new FreeTypeFontGenerator.FreeTypeFontParameter();
-		parameter.size = 18;
-		parameter.characters = FreeTypeFontGenerator.DEFAULT_CHARS;
-		font = generator.generateFont(parameter);
-
-		generator.dispose();
-		//Label.LabelStyle style = new Label.LabelStyle(font,font.getColor());
-		//Label a = new Label("a",style);
+		stage = new Stage();
+		BitmapFont pixel_font = create_font("pixel");
+		BitmapFont chinese_font = create_font("Chinese");
+		//img = new Texture("badlogic.jpg");
+		Editor editor = new Editor();
+		//Gdx.input.setInputProcessor(stage);
+		editor.create_project_view(stage,pixel_font);
+		editor.create_main_view(stage,chinese_font);
 		cam = new OrthographicCamera();
-		cam.setToOrtho(false, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-		cam.update();
+		//cam.setToOrtho(false, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		//cam.update();
+	}
+	public BitmapFont create_font(String type)
+	{
+		BitmapFont font = null;
+		if(type == "pixel") {
+			FreeTypeFontGenerator generator = new FreeTypeFontGenerator(Gdx.files.internal("m5x7.ttf"));
+			FreeTypeFontGenerator.FreeTypeFontParameter parameter = new FreeTypeFontGenerator.FreeTypeFontParameter();
+			parameter.size = 35;
+			parameter.characters = FreeTypeFontGenerator.DEFAULT_CHARS;
+			font = generator.generateFont(parameter);
+			generator.dispose();
+		}
+		if(type == "Chinese")
+		{
+			FreeTypeFontGenerator generator = new FreeTypeFontGenerator(Gdx.files.internal("pht.otf"));
+			FreeTypeFontGenerator.FreeTypeFontParameter parameter = new FreeTypeFontGenerator.FreeTypeFontParameter();
+			parameter.size = 35;
+			parameter.characters = FreeTypeFontGenerator.DEFAULT_CHARS + "按钮组";
+			font = generator.generateFont(parameter);
+			generator.dispose();
+		}
+		return font;
 	}
 
 	@Override
 	public void render () {
 		//Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.setProjectionMatrix(cam.combined);
+		//batch.setProjectionMatrix(cam.combined);
 		batch.begin();
+		stage.act();
+		stage.draw();
 		//batch.draw(img, 100, 100);
-		font.draw(batch,"Hello world 死角的数据",100,100);
+		//font.draw(batch,"Hello world 死角的数据",100,100);
 		batch.end();
 
 	}
-	
+	public void update()
+	{
+		System.out.println("处理输入");
+		}
 	@Override
 	public void dispose () {
 		batch.dispose();
